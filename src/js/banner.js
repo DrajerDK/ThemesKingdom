c3ef5fa58c90c197a6c4f2075987567e4ef51eb8
DrajerDK <141380564+DrajerDK@users.noreply.github.com>
2023-11-19 17:15:44 +0300

    feat(ThemesKingdom): add image in banner

---
+++ src/js/banner.js
@@ -0,0 +1,16 @@
+// let lastScrollTop = 0;
+
+// window.addEventListener("scroll", function() {
+//     let st = window.pageYOffset || document.documentElement.scrollTop;
+//     let videoElement = document.querySelector('.video-container__video');
+
+//     if (st > lastScrollTop) {
+//         // Прокрутка вниз
+//         videoElement.style.transform = 'translate(-50%, -50%) rotate(-5deg)';
+//     } else {
+//         // Прокрутка вверх
+//         videoElement.style.transform = 'translate(-50%, -50%) ';
+//     }
+
+//     lastScrollTop = st;
+// });
